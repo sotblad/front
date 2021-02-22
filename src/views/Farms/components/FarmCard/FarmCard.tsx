02c1bef8790f36c679fd4ed6af29fbdbf1bf0d08
baby-koala <monkeyfinance42@protonmail.com>
2021-02-22 12:24:09 +0100

    remove risk tag + highlight deposit fees

--- src/views/Farms/components/FarmCard/FarmCard.tsx
+++ src/views/Farms/components/FarmCard/FarmCard.tsx
@@ -167,9 +167,14 @@ const FarmCard: React.FC<FarmCardProps> = ({ farm, removed, cakePrice, bnbPrice,
         <Text>{TranslateString(318, 'Earn')}:</Text>
         <Text bold>{earnLabel}</Text>
       </Flex>
-      <Flex justifyContent="space-between">
-        <Text style={{ fontSize: '24px' }}>{TranslateString(10001, 'Deposit Fee')}:</Text>
-        <Text bold style={{ fontSize: '24px' }}>
+      <Flex justifyContent="space-between" >
+        <Text>{TranslateString(10001, 'Deposit Fee')}:</Text>
+        <Text bold style={{
+          backgroundColor: farm.depositFeeBP === 0 ? '#4bca4b' : 'transparent',
+          color: farm.depositFeeBP === 0 ? 'white' : '#424f3e',
+          padding: farm.depositFeeBP === 0 ? '2px 5px' : 'inherit',
+          borderRadius: farm.depositFeeBP === 0 ? '10px' : 'inherit',
+        }}>
           {farm.depositFeeBP / 100}%
         </Text>
       </Flex>
