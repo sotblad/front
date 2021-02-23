50bdba96e7170e879b926cde28b2022c6523cdeb
Baby Koala <78746937+baby-koala@users.noreply.github.com>
2021-02-23 16:22:44 +0100

    Update config.ts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -51,19 +51,15 @@ const config: MenuEntry[] = [
     items: [
       {
         label: 'PancakeSwap',
-        href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+        href: 'https://pancakeswap.info/token/0xba26397cdff25f0d26e815d218ef3c77609ae7f1',
       },
       // {
       //   label: 'CoinGecko',
-      //   href: 'https://www.coingecko.com/en/coins/goose-finance',
+      //   href: 'https://www.coingecko.com/en/coins/koalatoken',
       // },
       // {
       //   label: 'CoinMarketCap',
-      //   href: 'https://coinmarketcap.com/currencies/goose-finance/',
-      // },
-      // {
-      //   label: 'AstroTools',
-      //   href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
+      //   href: 'https://coinmarketcap.com/currencies/koalatoken/',
       // },
     ],
   },
