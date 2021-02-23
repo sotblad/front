f8425ba743eb58ffa8c33dd153efa7f3dbe55ceb
Baby Koala <78746937+baby-koala@users.noreply.github.com>
2021-02-23 15:38:19 +0100

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,10 +1,10 @@
 export default {
   cake: {
-    56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+    56: '0xba26397cdff25f0d26e815d218ef3c77609ae7f1',
     97: '',
   },
   masterChef: {
-    56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
+    56: '0x68c616DCeA206055B85830E641FBf7A20648548D',
     97: '',
   },
   wbnb: {
