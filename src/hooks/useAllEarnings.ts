77fadb6e01437639a032815760a524067e9e4cea
Baby Koala <78746937+baby-koala@users.noreply.github.com>
2021-02-23 16:01:20 +0100

    Update useAllEarnings.ts

--- src/hooks/useAllEarnings.ts
+++ src/hooks/useAllEarnings.ts
@@ -15,7 +15,7 @@ const useAllEarnings = () => {
     const fetchAllBalances = async () => {
       const calls = farmsConfig.map((farm) => ({
         address: getMasterChefAddress(),
-        name: 'pendingEgg',
+        name: 'pendingLyptus',
         params: [farm.pid, account],
       }))
 
